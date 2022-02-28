29f4d74f85a9673d72048a48cf57598df6ba34a0
syedislame <89494655+syedislame@users.noreply.github.com>
2022-02-27 21:51:10 -0800

    Add files via upload

--- QP_Project.ino
+++ QP_Project.ino
@@ -1,18 +1,28 @@
+#include <NewPing.h>
+#include <NewTone.h>
+
+
 #define echoPin 2 
 #define trigPin 3
 #define buzzpin 8
+#define maxdis 300
 
 long duration; 
-int distance; 
+int median;
+int distance;
+
+
+NewPing sonar(trigPin, echoPin, maxdis);
 
 void setup() {
   pinMode(trigPin, OUTPUT); 
   pinMode(echoPin, INPUT); 
   pinMode (buzzpin, OUTPUT);
   Serial.begin(9600); 
+
 }
 void loop() {
-  digitalWrite(trigPin, LOW);
+  /*digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -21,11 +31,26 @@ void loop() {
   distance = duration * 0.034 / 2; 
   Serial.print("Distance: ");
   Serial.print(distance);
-  Serial.println(" cm");
-  if (distance<60){
-    tone (buzzpin, 1000);
+  Serial.println(" cm");*/
+  delay(0);
+  unsigned int uS = sonar.ping();
+  Serial.print("Ping: ");
+  //Serial.print(sonar.convert_cm(uS));
+  //Serial.println("cm");
+  median = sonar.ping_median(10);
+  //Serial.println(times);
+  distance= sonar.convert_cm(median);
+  Serial.println(distance);
+  if (distance<60 && distance>0){
+    NewTone(buzzpin, 1000);
+    Serial.println("buzzer on");
   }
   if (distance>60){
-    noTone(buzzpin);
+    noNewTone(buzzpin);
+    Serial.println("buzzer off");
+  }
+  if (distance==0){
+    noNewTone(buzzpin);
+    Serial.println("buzzer off: 0");
   }
 }
